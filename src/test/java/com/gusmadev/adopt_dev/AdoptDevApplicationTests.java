4dfdef9632b000ad28baab15a5730f7fd6ac1b97
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-06-07 11:20:56 -0300

    first commit

---
+++ src/test/java/com/gusmadev/adopt_dev/AdoptDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gusmadev.adopt_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
