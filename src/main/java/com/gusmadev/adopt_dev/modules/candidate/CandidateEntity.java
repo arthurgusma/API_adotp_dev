2004b2d2faf14c5eaf073a4f66c4ee52822717b5
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-06-07 12:09:58 -0300

    feat: exceptions error handler

--- src/main/java/com/gusmadev/adopt_dev/modules/candidate/CandidateEntity.java
+++ src/main/java/com/gusmadev/adopt_dev/modules/candidate/CandidateEntity.java
@@ -1,6 +1,7 @@
 package com.gusmadev.adopt_dev.modules.candidate;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
@@ -11,14 +12,18 @@ import java.util.UUID;
 public class CandidateEntity {
 
     private UUID id;
+    @NotBlank(message = "Name should not be blank")
     private String name;
 
+    @NotBlank(message = "Username should not be blank")
     @Pattern(regexp = "\\S+", message = "Username should not contain spaces")
     private String username;
 
+    @NotBlank(message = "Email should not be blank")
     @Email(message = "Email should be valid")
     private String email;
 
+    @NotBlank(message = "Password should not be blank")
     @Length(min = 10, max = 100, message = "Password should be between 10 and 100 characters")
     private String password;
     private String description;
