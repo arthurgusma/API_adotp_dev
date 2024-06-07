2ad42eaed51eff0206e438be072e47dd9807f2d9
Arthur Gusmao <arthur.cabj@hotmail.com>
2024-06-07 11:39:17 -0300

    feat: adds lombok

--- src/main/java/com/gusmadev/adopt_dev/modules/candidate/CandidateEntity.java
+++ src/main/java/com/gusmadev/adopt_dev/modules/candidate/CandidateEntity.java
@@ -1,2 +1,17 @@
-package com.gusmadev.adopt_dev.modules.candidate;public class CandidateEntity {
+package com.gusmadev.adopt_dev.modules.candidate;
+
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class CandidateEntity {
+
+    private UUID id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    private String description;
+    private String curriculum;
 }
--- src/main/java/com/gusmadev/adopt_dev/modules/candidate/controller/CandidateController.java
+++ src/main/java/com/gusmadev/adopt_dev/modules/candidate/controller/CandidateController.java
@@ -1,2 +1,17 @@
-package com.gusmadev.adopt_dev.modules.candidate.controller;public class CandidateController {
+package com.gusmadev.adopt_dev.modules.candidate.controller;
+
+import com.gusmadev.adopt_dev.modules.candidate.CandidateEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateController {
+
+    @PostMapping("/")
+    public void create(@RequestBody CandidateEntity candidateEntity) {
+        System.out.println(candidateEntity.getName());
+    }
 }
